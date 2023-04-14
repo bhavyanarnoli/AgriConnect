d0fd65aabe1232b421409d44f0db6371ce5f1e63
mahanshAdtiya <mahansh21334@iiitd.ac.in>
2023-04-14 11:59:00 +0530

    Basic React Vite template Setup, Noothing more

---
+++ Client/src/components/HomeScreen.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function HomeScreen() {
+  return (
+    <div className='text-white'>HomeScreen</div>
+  )
+}
+
+export default HomeScreen
--- Client/src/constants/index.js
+++ Client/src/constants/index.js
@@ -25,20 +25,20 @@
 //     threejs,
 //   } from "../assets";
   
-//   export const navLinks = [
-//     {
-//       id: "about",
-//       title: "About",
-//     },
-//     {
-//       id: "work",
-//       title: "Work",
-//     },
-//     {
-//       id: "contact",
-//       title: "Contact",
-//     },
-//   ];
+  export const navLinks = [
+    {
+      id: "about",
+      title: "About",
+    },
+    // {
+    //   id: "work",
+    //   title: "Work",
+    // },
+    // {
+    //   id: "contact",
+    //   title: "Contact",
+    // },
+  ];
   
 //   const services = [
 //     {
